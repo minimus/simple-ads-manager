090ab51d83a940fda816cd229964cfe94c0a9ec4
Konstantin <minimus@simplelib.com>
2012-07-04 12:09:20 +0300

    Revert "Fixes by Latibro"
    This reverts commit e5541f3bf8feaef4af00db2d88a11a932c301256.

--- js/langs/en.js
+++
@@ -1,9 +0,0 @@
-tinyMCE.addI18n("en.samb", {
-	insert_samb : 'Insert Advertisement',
-  title: 'Insert Advertisement',
-  description: 'Select Advertisement Object',
-  ad: 'Insert Single Ad',
-  place: 'Insert Ads Place',
-  zone: 'Insert Ads Zone',
-  block: 'Insert Ads Block'
-});
--- js/sam-layout-2.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    $(".sam_ad").click(function() {
-      var adId = $(this).attr('id');
-      var data = { action: 'sam_click', sam_ad_id: adId, _ajax_nonce: samAjax._ajax_nonce };
-      jQuery.post(samAjax.ajaxurl, data, function(data){
-        alert("Data Loaded: " + data);
-      });
-    });
-  });
