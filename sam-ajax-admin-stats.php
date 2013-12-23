c6000c95dc65405f855a9f8fb0ad23597b2f7031
minimus <minimus@simplelib.com>
2013-12-23 11:46:18 +0300

    Fixing month bug of Statistic System

--- sam-ajax-admin-stats.php
+++ sam-ajax-admin-stats.php
@@ -57,18 +57,21 @@ if(in_array($action, $allowed_actions)) {
       if(isset($_POST['id'])) {
         $pid = $_POST['id'];
         $sMonth = (isset($_POST['sm'])) ? $_POST['sm'] : 0;
-        $now = getdate();
-        $month = $now['mon'] - $sMonth;
-        $year = $now['year'];
-        $days = samDaysInMonth($month, $year);
+
+        $date = new DateTime('now');
+        $si = '-'.$sMonth.' month';
+        $date->modify($si);
+        $month = $date->format('Y-m-d');
+        $days = $date->format('t');
+
         $sql = "SELECT
                   DATE_FORMAT(ss.event_time, %s) AS ed,
                   COUNT(*) AS hits
                 FROM $sTable ss
-                WHERE (EXTRACT(YEAR_MONTH FROM NOW()) - %d = EXTRACT(YEAR_MONTH FROM ss.event_time)) AND ss.event_type = %d AND ss.pid = %d
+                WHERE (EXTRACT(YEAR_MONTH FROM %s) = EXTRACT(YEAR_MONTH FROM ss.event_time)) AND ss.event_type = %d AND ss.pid = %d
                 GROUP BY ed;";
-        $hits = $wpdb->get_results($wpdb->prepare($sql, '%e', $sMonth, 0, $pid), ARRAY_A);
-        $clicks = $wpdb->get_results($wpdb->prepare($sql, '%e', $sMonth, 1, $pid), ARRAY_A);
+        $hits = $wpdb->get_results($wpdb->prepare($sql, '%e', $month, 0, $pid), ARRAY_A);
+        $clicks = $wpdb->get_results($wpdb->prepare($sql, '%e', $month, 1, $pid), ARRAY_A);
         for($i = 1; $i <= $days; $i++) {
           $hitsFull[$i - 1] = array( $i, 0);
           $clicksFull[$i - 1] = array($i, 0);
@@ -80,14 +83,14 @@ if(in_array($action, $allowed_actions)) {
                   (SELECT
                     COUNT(*)
                     FROM $sTable ss
-                    WHERE (EXTRACT(YEAR_MONTH FROM NOW()) - %d = EXTRACT(YEAR_MONTH FROM ss.event_time))
+                    WHERE (EXTRACT(YEAR_MONTH FROM %s) = EXTRACT(YEAR_MONTH FROM ss.event_time))
                       AND ss.event_type = 0 AND ss.pid = %d) AS hits,
                   (SELECT
                     COUNT(*)
                     FROM $sTable ss
-                    WHERE (EXTRACT(YEAR_MONTH FROM NOW()) - %d = EXTRACT(YEAR_MONTH FROM ss.event_time))
+                    WHERE (EXTRACT(YEAR_MONTH FROM %s) = EXTRACT(YEAR_MONTH FROM ss.event_time))
                       AND ss.event_type = 1 AND ss.pid = %d) AS clicks;";
-        $total = $wpdb->get_row($wpdb->prepare($sql, $sMonth, $pid, $sMonth, $pid));
+        $total = $wpdb->get_row($wpdb->prepare($sql, $month, $pid, $month, $pid));
 
         $out = array('hits' => $hitsFull, 'clicks' => $clicksFull, 'total' => $total);
       }
@@ -98,19 +101,21 @@ if(in_array($action, $allowed_actions)) {
       if(isset($_POST['id'])) {
         $id = $_POST['id'];
         $sMonth = (isset($_POST['sm'])) ? $_POST['sm'] : 0;
-        $now = getdate();
-        $month = $now['mon'] - $sMonth;
-        $year = $now['year'];
-        $days = samDaysInMonth($month, $year);
+
+        $date = new DateTime('now');
+        $si = '-'.$sMonth.' month';
+        $date->modify($si);
+        $month = $date->format('Y-m-d');
+        $days = $date->format('t');
 
         $sql = "SELECT
                   DATE_FORMAT(ss.event_time, %s) AS ed,
                   COUNT(*) AS hits
                 FROM $sTable ss
-                WHERE (EXTRACT(YEAR_MONTH FROM NOW()) - %d = EXTRACT(YEAR_MONTH FROM ss.event_time)) AND ss.event_type = %d AND ss.id = %d
+                WHERE (EXTRACT(YEAR_MONTH FROM %s) = EXTRACT(YEAR_MONTH FROM ss.event_time)) AND ss.event_type = %d AND ss.id = %d
                 GROUP BY ed;";
-        $hits = $wpdb->get_results($wpdb->prepare($sql, '%e', $sMonth, 0, $id), ARRAY_A);
-        $clicks = $wpdb->get_results($wpdb->prepare($sql, '%e', $sMonth, 1, $id), ARRAY_A);
+        $hits = $wpdb->get_results($wpdb->prepare($sql, '%e', $month, 0, $id), ARRAY_A);
+        $clicks = $wpdb->get_results($wpdb->prepare($sql, '%e', $month, 1, $id), ARRAY_A);
         for($i = 1; $i <= $days; $i++) {
           $hitsFull[$i - 1] = array( $i, 0);
           $clicksFull[$i - 1] = array($i, 0);
@@ -122,14 +127,14 @@ if(in_array($action, $allowed_actions)) {
                   (SELECT
                     COUNT(*)
                     FROM $sTable ss
-                    WHERE (EXTRACT(YEAR_MONTH FROM NOW()) - %d = EXTRACT(YEAR_MONTH FROM ss.event_time))
+                    WHERE (EXTRACT(YEAR_MONTH FROM %s) = EXTRACT(YEAR_MONTH FROM ss.event_time))
                       AND ss.event_type = 0 AND ss.id = %d) AS hits,
                   (SELECT
                     COUNT(*)
                     FROM $sTable ss
-                    WHERE (EXTRACT(YEAR_MONTH FROM NOW()) - %d = EXTRACT(YEAR_MONTH FROM ss.event_time))
+                    WHERE (EXTRACT(YEAR_MONTH FROM %s) = EXTRACT(YEAR_MONTH FROM ss.event_time))
                       AND ss.event_type = 1 AND ss.id = %d) AS clicks;";
-        $total = $wpdb->get_row($wpdb->prepare($sql, $sMonth, $id, $sMonth, $id));
+        $total = $wpdb->get_row($wpdb->prepare($sql, $month, $id, $month, $id));
 
         $out = array('hits' => $hitsFull, 'clicks' => $clicksFull, 'total' => $total);
       }
@@ -140,20 +145,27 @@ if(in_array($action, $allowed_actions)) {
       if(isset($_POST['id'])) {
         $id = $_POST['id'];
         $sMonth = (isset($_POST['sm'])) ? $_POST['sm'] : 0;
+
+        $date = new DateTime('now');
+        $si = '-'.$sMonth.' month';
+        $date->modify($si);
+        $month = $date->format('Y-m-d');
+        //$days = $date->format('t');
+
         $sql = "SELECT
                   sa.id,
                   sa.pid,
                   sa.name,
                   sa.description,
-                  @ad_hits := (SELECT COUNT(*) FROM $sTable ss WHERE (EXTRACT(YEAR_MONTH FROM NOW()) - %d = EXTRACT(YEAR_MONTH FROM ss.event_time)) AND ss.id = sa.id AND ss.pid = sa.pid AND ss.event_type = 0) AS ad_hits,
-                  @ad_clicks := (SELECT COUNT(*) FROM $sTable ss WHERE (EXTRACT(YEAR_MONTH FROM NOW()) - %d = EXTRACT(YEAR_MONTH FROM ss.event_time)) AND ss.id = sa.id AND ss.pid = sa.pid AND ss.event_type = 1) AS ad_clicks,
+                  @ad_hits := (SELECT COUNT(*) FROM $sTable ss WHERE (EXTRACT(YEAR_MONTH FROM %s) = EXTRACT(YEAR_MONTH FROM ss.event_time)) AND ss.id = sa.id AND ss.pid = sa.pid AND ss.event_type = 0) AS ad_hits,
+                  @ad_clicks := (SELECT COUNT(*) FROM $sTable ss WHERE (EXTRACT(YEAR_MONTH FROM %s) = EXTRACT(YEAR_MONTH FROM ss.event_time)) AND ss.id = sa.id AND ss.pid = sa.pid AND ss.event_type = 1) AS ad_clicks,
                   sa.ad_weight,
                   (sa.cpm / @ad_hits * 1000) AS e_cpm,
                   sa.cpc AS e_cpc,
                   (@ad_clicks / @ad_hits * 100) AS e_ctr
                 FROM $aTable sa
                 WHERE (sa.pid = %d) AND sa.trash = FALSE AND NOT (sa.ad_schedule AND sa.ad_end_date < NOW());";
-        $rows = $wpdb->get_results($wpdb->prepare($sql, $sMonth, $sMonth, $id), ARRAY_A);
+        $rows = $wpdb->get_results($wpdb->prepare($sql, $month, $month, $id), ARRAY_A);
 
         $k = 0;
         foreach($rows as &$row) {
